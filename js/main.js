2958c0a0d513189d906c07a4869b212ef44b1edb
Daniel Fornells <daniel.fornellsgarcia@sundiogroup.com>
2017-02-22 22:18:17 +0100

    Starting the site

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+// More info https://github.com/hakimel/reveal.js#configuration
+Reveal.initialize({
+    history: true,
+
+    // More info https://github.com/hakimel/reveal.js#dependencies
+    dependencies: [
+        { src: 'plugin/markdown/marked.js' },
+        { src: 'plugin/markdown/markdown.js' },
+        { src: 'plugin/notes/notes.js', async: true },
+        { src: 'plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } }
+    ]
+});
