f1ec40eeba29d67628ca49d9f77016f28f2fbf61
Daniel Fornells <daniel.fornellsgarcia@sundiogroup.com>
2017-02-27 00:17:22 +0100

    Ready until BEMIT

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
+/*
+* ---------------------------------------
+* INIT REVEAL JS
+*
+* */
 
 // More info https://github.com/hakimel/reveal.js#configuration
 Reveal.initialize({
@@ -5,9 +10,36 @@ Reveal.initialize({
 
     // More info https://github.com/hakimel/reveal.js#dependencies
     dependencies: [
-        { src: 'plugin/markdown/marked.js' },
-        { src: 'plugin/markdown/markdown.js' },
-        { src: 'plugin/notes/notes.js', async: true },
-        { src: 'plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } }
+        { src: 'js/plugin/markdown/marked.js' },
+        { src: 'js/plugin/markdown/markdown.js' },
+        { src: 'js/plugin/notes/notes.js', async: true },
+        { src: 'js/plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } }
+
     ]
+        //{ src: 'plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } }
 });
+
+
+/*
+ * ---------------------------------------
+ * OTHER JS
+ *
+ * */
+
+var DF = window.DF = window.DF || {};
+
+DF.lazySource = function(){
+    window.setTimeout(function(){
+        var lazyElements = document.querySelectorAll('[lazy-src]');
+        for (var i = 0, n = lazyElements.length; i < n; i++)
+        {
+            lazyElements[i].setAttribute('src', lazyElements[i].getAttribute('lazy-src') );
+        }
+    }, 1000);
+};
+
+if (document.readyState != 'loading') {
+    DF.lazySource();
+} else {
+    document.addEventListener('DOMContentLoaded', DF.lazySource);
+}
